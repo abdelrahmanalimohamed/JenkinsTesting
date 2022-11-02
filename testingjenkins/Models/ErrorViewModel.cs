ce8edbafb97fb0a89159b221bc1e082b59c0c912
abdelrahmanali.mohamed@outlook.com <abdelrahmanali.mohamed@outlook.com>
2022-11-02 14:30:48 +0200

    initial

---
+++ testingjenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testingjenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
